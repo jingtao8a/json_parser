7bcea8dc46eaecf7360eedefd3cb6f35ad0c94f8
yuxintao <1921056015@qq.com>
2023-04-07 21:23:40 +0800

    finish tutorial 2

--- include/leptjson.h
+++ include/leptjson.h
@@ -4,6 +4,7 @@
 typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
 
 typedef struct {
+    double n;
     lept_type type;
 }lept_value;//节点
 
@@ -11,11 +12,14 @@ enum {
     LEPT_PARSE_OK = 0,
     LEPT_PARSE_EXPECT_VALUE,//只有空白
     LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR
+    LEPT_PARSE_ROOT_NOT_SINGULAR,
+    LEPT_PARSE_NUMBER_TOO_BIG,
 };//解析json时的返回值
 
 int lept_parse(lept_value* v, const char* json);//解析json
+
 lept_type lept_get_type(const lept_value* v);
 
+double lept_get_number(const lept_value* v);
 
 #endif
