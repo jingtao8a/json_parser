3616dcc61a1c16c0a9c3fdc6dafe44362a0ed12c
yuxintao <1921056015@qq.com>
2023-04-07 11:45:14 +0800

    finish tutorial 1

---
+++ include/leptjson.h
@@ -0,0 +1,21 @@
+#ifndef LEPTJSON_H__
+#define LEPTJSON_H__
+
+typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
+
+typedef struct {
+    lept_type type;
+}lept_value;//节点
+
+enum {
+    LEPT_PARSE_OK = 0,
+    LEPT_PARSE_EXPECT_VALUE,//只有空白
+    LEPT_PARSE_INVALID_VALUE,
+    LEPT_PARSE_ROOT_NOT_SINGULAR
+};//解析json时的返回值
+
+int lept_parse(lept_value* v, const char* json);//解析json
+lept_type lept_get_type(const lept_value* v);
+
+
+#endif
